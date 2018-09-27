56c5690b1c7773fd8c1517840ea452c401fa0c99
kevintudiep <kevintudiep@gmail.com>
2018-09-27 16:14:56 -0500

    Completed Exercise

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,31 @@
+class EntriesController < ApplicationController
+
+  def index
+    render :index
+  end
+
+  def show
+
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    redirect_to entry_url(params:[:id])
+  end
+
+  def destroy
+    redirect_to :index
+  end
+
+end
